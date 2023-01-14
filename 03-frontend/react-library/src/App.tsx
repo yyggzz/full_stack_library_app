dd92ce7a5b1e13aa15d91211fc8f8fc17faac35d
Yige <tjzhangyige@gmail.com>
2023-01-13 19:02:38 -0700

    init

---
+++ 03-frontend/react-library/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <ExploreTopBooks/>
+    </div>
+    
+  );
+}
+
+export default App;
