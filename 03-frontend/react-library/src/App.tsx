f44a20a55fc84f955dbe2c48849912db5a4d20ab
Yige <tjzhangyige@gmail.com>
2023-01-24 13:21:25 -0700

    React book checkout page

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -5,7 +5,7 @@ import { HomePage } from "./layouts/HomePage/HomePage";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
-
+import { BookCheckoutPage } from "./layouts/BookCheckoutPage/BookCheckoutPage";
 export const App = () => {
   return (
     <div className="d-flex flex-column min-vh-100">
@@ -20,6 +20,9 @@ export const App = () => {
           </Route>
           <Route path="/search">
             <SearchBooksPage />
+          </Route>
+          <Route path={"/checkout/:bookId"}>
+            <BookCheckoutPage />
           </Route>
         </Switch>
       </div>
