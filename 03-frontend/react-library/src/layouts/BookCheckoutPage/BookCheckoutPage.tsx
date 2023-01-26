7decdffdebf72ae2703d2aa49021c21c83936aa9
Yige <tjzhangyige@gmail.com>
2023-01-26 16:18:00 -0700

    Add React Book Review

--- 03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ 03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -2,6 +2,7 @@ import { useOktaAuth } from "@okta/okta-react";
 import { useEffect, useState } from "react";
 import BookModel from "../../models/BookModel";
 import ReviewModel from "../../models/ReviewModel";
+import ReviewRequestModel from "../../models/ReviewRequestModel";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
 import { StarsReview } from "../Utils/StarsReview";
 import { CheckoutAndReviewBox } from "./CheckoutAndReviewBox";
@@ -18,6 +19,9 @@ export const BookCheckoutPage = () => {
   const [reviews, setReviews] = useState<ReviewModel[]>([]);
   const [totalStars, setTotalStars] = useState(0);
   const [isLoadingReview, setIsLoadingReview] = useState(true);
+
+  const [isReviewLeft, setIsReviewLeft] = useState(false);
+  const [isLoadingUserReview, setIsLoadingUserReview] = useState(true);
 
   // Loans Count State
   const [currentLoansCount, setCurrentLoansCount] = useState(0);
@@ -103,7 +107,34 @@ export const BookCheckoutPage = () => {
       setIsLoadingReview(false);
       setHttpError(error.message);
     });
-  }, []);
+  }, [isReviewLeft]);
+
+  // User review
+  useEffect(() => {
+    const fetchUserReviewBook = async () => {
+      if (authState && authState.isAuthenticated) {
+        const url = `http://localhost:8080/api/reviews/secure/user/book/?bookId=${bookId}`;
+        const requestOptions = {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+            "Content-Type": "application/json",
+          },
+        };
+        const userReview = await fetch(url, requestOptions);
+        if (!userReview.ok) {
+          throw new Error("Something went wrong");
+        }
+        const userReviewResponseJson = await userReview.json();
+        setIsReviewLeft(userReviewResponseJson);
+      }
+      setIsLoadingUserReview(false);
+    };
+    fetchUserReviewBook().catch((error: any) => {
+      setIsLoadingUserReview(false);
+      setHttpError(error.message);
+    });
+  });
 
   // fetch loans count
   useEffect(() => {
@@ -167,7 +198,8 @@ export const BookCheckoutPage = () => {
     isLoading ||
     isLoadingReview ||
     isLoadingCurrentLoansCount ||
-    isLoadingBookCheckedOut
+    isLoadingBookCheckedOut ||
+    isLoadingUserReview
   ) {
     return <SpinnerLoading />;
   }
@@ -195,6 +227,32 @@ export const BookCheckoutPage = () => {
       throw new Error("Something went wrong!");
     }
     setIsCheckedOut(true);
+  }
+
+  async function submitReview(starInput: number, reviewDescription: string) {
+    let bookId: number = 0;
+    if (book?.id) {
+      bookId = book.id;
+    }
+    const reviewRequestModel = new ReviewRequestModel(
+      starInput,
+      bookId,
+      reviewDescription
+    );
+    const url = `http://localhost:8080/api/reviews/secure`;
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(reviewRequestModel),
+    };
+    const returnResponse = await fetch(url, requestOptions);
+    if (!returnResponse.ok) {
+      throw new Error("Something went wrong");
+    }
+    setIsReviewLeft(true);
   }
 
   return (
@@ -228,6 +286,8 @@ export const BookCheckoutPage = () => {
             isAuthenticated={authState?.isAuthenticated}
             isCheckedOut={isCheckedOut}
             checkoutBook={checkoutBook}
+            isReviewLeft={isReviewLeft}
+            submitReview={submitReview}
           />
         </div>
         <hr />
@@ -261,6 +321,8 @@ export const BookCheckoutPage = () => {
           isAuthenticated={authState?.isAuthenticated}
           isCheckedOut={isCheckedOut}
           checkoutBook={checkoutBook}
+          isReviewLeft={isReviewLeft}
+          submitReview={submitReview}
         />
         <hr />
         <LatestReviews reviews={reviews} bookId={book?.id} mobile={true} />
--- 03-frontend/react-library/src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
+++ 03-frontend/react-library/src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import BookModel from "../../models/BookModel";
+import { LeaveAReview } from "../Utils/LeaveAReview";
 
 export const CheckoutAndReviewBox: React.FC<{
   book: BookModel | undefined;
@@ -8,6 +9,8 @@ export const CheckoutAndReviewBox: React.FC<{
   isAuthenticated: any;
   isCheckedOut: boolean;
   checkoutBook: any;
+  isReviewLeft: boolean;
+  submitReview: any;
 }> = (props) => {
   function buttonRender() {
     if (props.isAuthenticated) {
@@ -37,34 +40,58 @@ export const CheckoutAndReviewBox: React.FC<{
     );
   }
 
+  function reviewRender() {
+    if (props.isAuthenticated && !props.isReviewLeft) {
+      return (
+        <p>
+          <LeaveAReview submitReview={props.submitReview} />
+        </p>
+      );
+    } else if (props.isAuthenticated && props.isReviewLeft) {
+      return (
+        <p>
+          <b>Thank you for your review!</b>
+        </p>
+      );
+    }
+    return (
+      <div>
+        <hr />
+        <p>Sign in to be able to leave a review.</p>
+      </div>
+    );
+  }
+
   return (
     <div
       className={
         props.mobile ? "card d-flex mt-5" : "card col-3 container d-flex mb-5"
       }
     >
-      <div className="mt-3">
-        <p>
-          <b>{props.currentLoansCount}/5 </b>
-          books checked out
-        </p>
-        <hr />
-        {props.book &&
-        props.book.copiesAvailable &&
-        props.book.copiesAvailable > 0 ? (
-          <h4 className="text-success">Available</h4>
-        ) : (
-          <h4 className="text-danger">Wait List</h4>
-        )}
-        <div className="row">
-          <p className="col-6 lead">
-            <b>{props.book?.copies} </b>
-            copies
+      <div className="card-body container">
+        <div className="mt-3">
+          <p>
+            <b>{props.currentLoansCount}/5 </b>
+            books checked out
           </p>
-          <p className="col-6 lead">
-            <b>{props.book?.copiesAvailable} </b>
-            available
-          </p>
+          <hr />
+          {props.book &&
+          props.book.copiesAvailable &&
+          props.book.copiesAvailable > 0 ? (
+            <h4 className="text-success">Available</h4>
+          ) : (
+            <h4 className="text-danger">Wait List</h4>
+          )}
+          <div className="row">
+            <p className="col-6 lead">
+              <b>{props.book?.copies} </b>
+              copies
+            </p>
+            <p className="col-6 lead">
+              <b>{props.book?.copiesAvailable} </b>
+              available
+            </p>
+          </div>
         </div>
         {buttonRender()}
 
@@ -72,7 +99,7 @@ export const CheckoutAndReviewBox: React.FC<{
         <p className="mt-3">
           This number can change until placing order has been complete.
         </p>
-        <p>Sign in to be able to leave a review.</p>
+        {reviewRender()}
       </div>
     </div>
   );
